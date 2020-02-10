e27f690ad4f42aded0748d533cc9090bce9557bb
Alvin-22 <alvinwtf2000@gmail.com>
2020-02-10 14:50:44 +0100

    wrong quotation type

--- domestic/utility/send_email.py
+++ domestic/utility/send_email.py
@@ -4,7 +4,7 @@ import smtplib
 def send_email(sender, sender_pw, recievers, subject, text):
   message = f'From: {sender}\nTo: {recievers}\nSubject: {subject}\n\n{text}'
 
-  server = smtplib.SMTP("smtp.gmail.com", 587)
+  server = smtplib.SMTP('smtp.gmail.com', 587)
   server.ehlo()
   server.starttls()
   server.login(sender, sender_pw)
